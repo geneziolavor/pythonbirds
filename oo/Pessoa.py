cd22a2dcee6f5fa372985f6e7612ec3380bc62d1
geneziolavor <35356028+geneziolavor@users.noreply.github.com>
2019-10-24 09:21:45 -0300

    criado atributo complexo filhos

--- oo/Pessoa.py
+++ oo/Pessoa.py
@@ -1,11 +1,23 @@
 class Pessoa:
+    def __init__(self, *filhos, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
     def cumprimentar(self):
         return f'Olá{id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
+    genezio = Pessoa(nome='genezio')
+    maria = Pessoa(genezio, nome='maria')
+    print(Pessoa.cumprimentar(genezio))
+    print(id(genezio))
+    print(genezio.cumprimentar())
+    print(genezio.nome)
+    print(genezio.idade)
+    for filho in genezio.filhos:
+        print(filho.nome)
+
+
+
 
 
